35ed8635d07f9798ba7a5165e447d237dde7cf27
overcomer <pierpaolo.monaco@gmail.com>
2020-05-06 17:53:22 +0200

    Add auth form validation

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from './auth.service';
-import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 import { LoadingController } from '@ionic/angular';
+import { Router } from '@angular/router';
+
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -32,5 +34,9 @@ export class AuthPage implements OnInit {
           this.router.navigateByUrl('/places/tabs/discover');
         }, 1500);
       });
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
   }
 }
