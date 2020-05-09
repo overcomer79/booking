ae38bff0f1cab906f5ba9067ba50fc451580da29
overcomer <pierpaolo.monaco@gmail.com>
2020-05-09 11:24:56 +0200

    Add filter for bookable places

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SegmentChangeEventDetail } from '@ionic/core';
 import { Subscription } from 'rxjs';
+import { MenuController } from '@ionic/angular';
 
 import { PlacesService } from '../places.service';
 import { Place } from '../place.model';
+import { AuthService } from '../../../app/auth/auth.service';
 
 @Component({
   selector: 'app-discover',
@@ -13,19 +15,34 @@ import { Place } from '../place.model';
 export class DiscoverPage implements OnInit, OnDestroy {
   loadedPlaces: Place[];
   listedLoadadPlaces: Place[];
+  releventPlaces: Place[];
   private placesSub: Subscription;
 
-  constructor(private placesService: PlacesService) {}
+  constructor(
+    private placesService: PlacesService,
+    private menuCtrl: MenuController,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
     this.placesSub = this.placesService.places.subscribe((places) => {
       this.loadedPlaces = places;
-      this.listedLoadadPlaces = this.loadedPlaces.slice(1);
+      this.releventPlaces = this.loadedPlaces;
+      this.listedLoadadPlaces = this.releventPlaces.slice(1);
     });
   }
 
+  onOpenMenu() {
+    this.menuCtrl.toggle();
+  }
+
   onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>) {
-    console.log(event.detail);
+    if (event.detail.value === 'all') {
+      this.releventPlaces = this.loadedPlaces;
+    } else {
+      this.releventPlaces = this.loadedPlaces.filter( place => place.userId  !== this.authService.userId);
+    }
+    this.listedLoadadPlaces = this.releventPlaces.slice(1);
   }
 
   ngOnDestroy(): void {
